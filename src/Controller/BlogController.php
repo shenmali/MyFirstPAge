ff42d636318ffab1fb1df07b84e30d35a793282c
malishen <muhalishen@gmail.com>
2021-10-13 10:29:19 +0300

    remove Doctrine

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+     /**
+     * @Route("/lucky/number/{id}", methods={"GET","HEAD"})
+     */
+    public function number(int $id)
+    {
+        $number = $id;
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+    public function numberYaml(int $id)
+    {
+        $number = $id;
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
